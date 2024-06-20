8b9b05feaaada6d4390529980768b3fbfa719bc3
promotheous-coder <madhav.p.akv@outlook.com>
2024-06-21 00:01:40 +0530

    Adding Bootstrap Data

---
+++ src/main/java/com/springframework/springwebapp/bootstrap/BootstrapData.java
@@ -0,0 +1,44 @@
+package com.springframework.springwebapp.bootstrap;
+
+import com.springframework.springwebapp.domain.Author;
+import com.springframework.springwebapp.domain.Book;
+import com.springframework.springwebapp.repository.AuthorRepository;
+import com.springframework.springwebapp.repository.BookRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BootstrapData implements CommandLineRunner {
+
+    private final BookRepository bookRepository;
+    private final AuthorRepository authorRepository;
+
+    public BootstrapData(BookRepository bookRepository, AuthorRepository authorRepository) {
+        this.bookRepository = bookRepository;
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author("Eric", "Evans");
+        Book ddd = new Book("Data Driven Design", "123123");
+
+        eric.getBooks().add(ddd);
+        ddd.getAuthors().add(eric);
+
+        authorRepository.save(eric);
+        bookRepository.save(ddd);
+
+        Author rod = new Author("Rod", "Johnson");
+        Book noEJB = new Book("J2EE Development without EJB", "123456788");
+
+        rod.getBooks().add(noEJB);
+        noEJB.getAuthors().add(rod);
+
+        authorRepository.save(rod);
+        bookRepository.save(noEJB);
+
+        System.out.println("Started in Bootstrap");
+        System.out.println("Number of Books: " + bookRepository.count());
+    }
+}
--- src/main/java/com/springframework/springwebapp/domain/Book.java
+++ src/main/java/com/springframework/springwebapp/domain/Book.java
@@ -2,6 +2,7 @@ package com.springframework.springwebapp.domain;
 
 import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
@@ -14,15 +15,14 @@ public class Book {
 
     @ManyToMany
     @JoinTable(name = "author_book",joinColumns = @JoinColumn(name = "book_id"),inverseJoinColumns = @JoinColumn(name = "author_id"))
-    private Set<Author> authors;
+    private Set<Author> authors = new HashSet<>();
 
     public Book() {
     }
 
-    public Book(String title, String isbn, Set<Author> authors) {
+    public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
-        this.authors = authors;
     }
 
     public Long getId() {
