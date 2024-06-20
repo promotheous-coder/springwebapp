630c615819507602f81e9595ecd7a3ca090b1c74
promotheous-coder <madhav.p.akv@outlook.com>
2024-06-20 22:39:09 +0530

    Equality in Hibernate

--- src/main/java/com/springframework/springwebapp/domain/Book.java
+++ src/main/java/com/springframework/springwebapp/domain/Book.java
@@ -56,4 +56,16 @@ public class Book {
     public void setAuthors(Set<Author> authors) {
         this.authors = authors;
     }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    
 }
